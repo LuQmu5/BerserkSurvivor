308f459b38775bcb08aaad5ede38e41c0730a0c6
LuQmu5 <135509819+LuQmu5@users.noreply.github.com>
2024-11-30 21:01:09 +0400

    1

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour, IHealth
 {
+    [SerializeField] private Transform _player;
+    [SerializeField] private NavMeshAgent _agent;
+
     public float MaxHealth { get; private set; } = 100;
     public float CurrentHealth { get; private set; }
 
     private void Start()
     {
         CurrentHealth = MaxHealth;
+    }
+
+    private void Update()
+    {
+        _agent.SetDestination(_player.position);
     }
 
     public void ApplyDamage(float amount)
